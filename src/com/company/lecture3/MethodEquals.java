5357bf608fdd9ee12feb477957e77ce83da9b6e6
IvanLapa <61242303+IvanLapa@users.noreply.github.com>
2020-02-27 11:28:12 +0300

    старт 3.5

--- src/com/company/lecture3/MethodEquals.java
+++ src/com/company/lecture3/MethodEquals.java
@@ -2,8 +2,13 @@ package com.company.lecture3;
 
 public class MethodEquals {
     public static void main(String[] args) {
-        System.out.print("IN WORK");
         int [][] array1 = {{1,2,3},{1,2,3}};
         int [][] array2 = {{3,2,1},{3,2,1}};
+            int i=0;
+            int j=0;
+            boolean answer;
+            if (array1[i][j] == array2[i][j]){
+                answer = true;
+            }
     }
 }
