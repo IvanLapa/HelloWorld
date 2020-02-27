aee534b40b46ee958c07bb66db4347e7efddec6d
IvanLapa <61242303+IvanLapa@users.noreply.github.com>
2020-02-27 10:37:01 +0300

    Задача 2/6 полностью выполненна

--- src/com/company/lecture3/Lecture3IntegerNumber.java
+++ src/com/company/lecture3/Lecture3IntegerNumber.java
@@ -6,54 +6,17 @@ public class Lecture3IntegerNumber {
     public static void main(String[] args) {
         System.out.println("Введите число больше 1:");
         Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
-        String str1 = " является простым числом";
-        String str2 = " не является простым числом";
-        switch (i) {
-            case 2:
-                System.out.println(i + str1);
-                break;
-            case 3:
-                System.out.println(i + str1);
-                break;
-            case 5:
-                System.out.println(i + str1);
-                break;
-            case 7:
-                System.out.println(i + str1);
-                break;
-            case 11:
-                System.out.println(i + str1);
-                break;
-            case 13:
-                System.out.println(i + str1);
-                break;
-            case 17:
-                System.out.println(i + str1);
-                break;
-            case 19:
-                System.out.println(i + str1);
-                break;
-            default:
-        if (i % 2 == 0) {
-                System.out.println(i + str2);
-            }   else if (i % 3 == 0) {
-                System.out.println(i + str2);
-            }   else if (i % 5 == 0) {
-                System.out.println(i + str2);
-            }   else if (i % 7 == 0) {
-                System.out.println(i + str2);
-            }   else if (i % 11 == 0) {
-                System.out.println(i + str2);
-            }   else if (i % 13 == 0) {
-                System.out.println(i + str2);
-            }   else if (i % 17 == 0) {
-                System.out.println(i + str2);
-            }   else if (i % 19 == 0) {
-                System.out.println(i + str2);
-            }   else {
-                System.out.println(i + str1);
+        int number = sc.nextInt();
+        int number_dividers=0;
+        for(int i=1; i<number; i++){
+            if(number%i==0) {
+                number_dividers++;
             }
+        }
+        if(number_dividers==1){
+            System.out.println(number + " простое число");
+        }else{
+            System.out.println(number + " составное число");
         }
     }
 }
--- src/com/company/lecture3/Lecture3MethodEquals.java
+++ src/com/company/lecture3/Lecture3MethodEquals.java
@@ -3,5 +3,7 @@ package com.company.lecture3;
 public class Lecture3MethodEquals {
     public static void main(String[] args) {
         System.out.print("IN WORK");
+        int [][] array1 = {{1,2,3},{1,2,3}};
+        int [][] array2 = {{3,2,1},{3,2,1}};
     }
 }
