a0da413e67270b1f059b8bfc71c19b4f73a40384
IvanLapa <61242303+IvanLapa@users.noreply.github.com>
2020-02-27 12:16:09 +0300

    старт 3.5

--- src/com/company/lecture3/MethodEquals.java
+++ src/com/company/lecture3/MethodEquals.java
@@ -5,7 +5,8 @@ public class MethodEquals {
         int[][] array1 = {{1, 2, 3}, {1, 2, 3}};
         int[][] array2 = {{3, 2, 1}, {3, 2, 1}};
 
-        public static boolean equals (array1[][], array2[][], int i; int j;) {
+        public static boolean equals (array1[][],array2[][],int i;
+        int j;){
             i = 0;
             j = 0;
             boolean answer;
