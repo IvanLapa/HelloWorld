2c5bab4c5bf4b10d0de2e17e3d7443ed9fffc5a2
IvanLapa <61242303+IvanLapa@users.noreply.github.com>
2020-04-04 20:55:26 +0300

    коллекции часть первая (занятие 10) - 6 задачу вообш без понятия как делать(( 7 - есть понимание, но чувтвую что потрачу пол дня на ней)) Поэтому с точки зрения средней оценки за курс - перейду к ледующей домашке. Если еще время будет позволять - вернусь к долгам.

---
+++ src/com/company/lecture10/Mission2.java
@@ -0,0 +1,28 @@
+package com.company.lecture10;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
+
+public class Mission2 {
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        ListIterator<Integer> listIterator = list.listIterator();
+        Random mark = new Random();
+        for (int i = 0; i < 10; i++) {
+            listIterator.add(mark.nextInt(10));
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+        listIterator = list.listIterator();
+        int max_mark = 0;
+        while (listIterator.hasNext()) {
+            Integer match_mark = listIterator.next();
+            if (match_mark >= max_mark) {
+                max_mark = match_mark;
+            }
+        }
+        System.out.println(max_mark);
+    }
+}
