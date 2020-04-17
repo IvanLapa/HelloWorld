6530eb1a7bbb083f61b0956d1082a7fadd11da66
IvanLapa <61242303+IvanLapa@users.noreply.github.com>
2020-04-17 23:39:13 +0300

    Date API

---
+++ Test/calculator/CalculatorTest.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import com.company.lecture30JUnit.Calculator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+    @Test
+    public void addTest(){
+        Calculator <Integer> calculator = new Calculator<>(3, 3);
+        Assert.assertEquals("Incorrect sum", 6, calculator.add());
+        calculator.setArg1(-1);
+        calculator.setArg2(3);
+        Assert.assertEquals("Incorrect sum", 2, calculator.add());
+        calculator.setArg1(4);
+        calculator.setArg2(2);
+        Assert.assertEquals("Incorrect", 2, calculator.minus());
+    }
+    @Test
+    public void addDoubleTest(){
+        Calculator <Double> calculator = new Calculator<>(3., 3.);
+        Assert.assertEquals("Incorrect sum", 6, calculator.add());
+        calculator.setArg1(-1.);
+        calculator.setArg2(3.);
+        Assert.assertEquals("Incorrect", -4, calculator.minus());
+    }
+}
