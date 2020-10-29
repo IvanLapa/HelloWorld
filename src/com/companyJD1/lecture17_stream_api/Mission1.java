6785ffe6b809a4ad1c5e1fd4735c7878a4db951d
IvanLapa <61242303+IvanLapa@users.noreply.github.com>
2020-10-30 01:40:36 +0300

    Commit

--- src/com/companyJD1/lecture17_stream_api/Mission1.java
+++ src/com/companyJD1/lecture17_stream_api/Mission1.java
@@ -3,14 +3,21 @@ package com.companyJD1.lecture17_stream_api;
 import java.util.Arrays;
 import java.util.List;
 
+/**Дан список целых чисел.
+ * Найти среднее всех нечётных чисел, делящихся на 5.
+ */
+
 public class Mission1 {
     public static void main(String[] args) {
+
         List<Integer> numbers = Arrays.asList(25, 15, 6, 35, 45);
+
         double result = numbers.stream()
                 .filter((number) -> number!=null && number%2!=0 && number%5==0)
                 .mapToLong(element -> element)
                 .average()
                 .orElse(0);
+
         System.out.println(result);
     }
 }
--- src/com/companyJD1/lecture17_stream_api/Mission2.java
+++ src/com/companyJD1/lecture17_stream_api/Mission2.java
@@ -3,10 +3,20 @@ package com.companyJD1.lecture17_stream_api;
 import java.util.Arrays;
 import java.util.List;
 
+/**Дан список строк.
+ * Найти количество уникальных строк длиной более 8 символов.
+ */
+
 public class Mission2 {
     public static void main(String[] args) {
+
     List<String> string_list = Arrays.asList("Hello Ivan", "world is mine", "hello", "Ivan");
-    long count = string_list.stream().filter(str->str.length()>8).distinct().count();
-        System.out.println(count);
+
+    long count = string_list.stream()
+            .filter(str->str.length()>8)
+            .distinct()
+            .count();
+
+    System.out.println(count);
     }
 }
