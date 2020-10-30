aaef8fda0e69523693dfc58ebb50cc7ac4e8740e
IvanLapa <61242303+IvanLapa@users.noreply.github.com>
2020-10-30 14:37:14 +0300

    Commit

--- Test/calculator/CalculatorTest.java
+++ Test/calculator/CalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorTest {
+
     @Test
     public void addTest(){
         Calculator <Integer> calculator = new Calculator<>(3, 3);
@@ -16,9 +17,10 @@ public class CalculatorTest {
         calculator.setArg2(2);
         Assert.assertEquals("Incorrect", 2, calculator.minus());
     }
+
     @Test
     public void addDoubleTest(){
-        Calculator <Double> calculator = new Calculator<>(3., 3.);
+        Calculator <Double> calculator = new Calculator<>(3.0, 3.0);
         Assert.assertEquals("Incorrect sum", 6, calculator.add());
         calculator.setArg1(-1.);
         calculator.setArg2(3.);
